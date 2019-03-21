9c29f2b26fe11542cd4d357b8f700ab7da1c1156
durikang <kooda21@naver.com>
2019-03-21 12:25:38 +0900

    다이아 별찍기

---
+++ 다이아찍기/다이아찍기/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 다이아찍기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+
+                for (int j = 5; j - i > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = i; j < i + i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                    for (int j = i; j < i + i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    
+
+                
+                Console.WriteLine();
+            }
+            
+            for(int i = 0; i < 5; i++)
+            {
+
+                for (int j = 0; j < i+1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 5; j - i > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 4; j - i > 0; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
